509fa377e63f5181b9fd4e6078d98205730400c9
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-02-18 22:47:32 +0100

    mas animation en fadein y fadeout

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,16 +1,24 @@
 import ThemeButton from "@/components/ui/ThemeButton";
 import ThemeView from "@/components/ui/ThemeView";
 import { useRef } from "react";
-import { Animated, View } from "react-native";
+import { Animated, Easing, View } from "react-native";
 
 const Animation101Screen = () => {
   const animatedOpacity = useRef(new Animated.Value(0)).current;
+  const animatedTop = useRef(new Animated.Value(-80)).current;
 
   const fadeIn = () => {
     Animated.timing(animatedOpacity, {
       toValue: 1,
       duration: 300,
       useNativeDriver: true,
+    }).start();
+
+    Animated.timing(animatedTop, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+      easing: Easing.bounce,
     }).start();
   };
 
@@ -20,13 +28,19 @@ const Animation101Screen = () => {
       duration: 300,
       useNativeDriver: true,
     }).start();
+
+    Animated.timing(animatedTop, {
+      toValue: -80,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
   };
 
   return (
     <ThemeView margin className="flex-1 items-center justify-center gap-y-5">
       <Animated.View
         className="bg-light-secondary dark:bg-dark-secondary rounded-xl w-[100px] h-[100px]"
-        style={{ opacity: animatedOpacity }}
+        style={{ opacity: animatedOpacity, transform: [{ translateY: animatedTop }] }}
       />
 
       <ThemeButton onPress={fadeIn}>FadeIn</ThemeButton>
