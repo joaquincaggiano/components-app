b4280e43671d2f1bb5e989581c2577dfd5857157
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-02-18 22:37:52 +0100

    fadein and fadeout animation

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,11 +1,36 @@
 import ThemeButton from "@/components/ui/ThemeButton";
 import ThemeView from "@/components/ui/ThemeView";
+import { useRef } from "react";
+import { Animated, View } from "react-native";
 
 const Animation101Screen = () => {
+  const animatedOpacity = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
-    <ThemeView margin className="gap-y-4">
-      <ThemeButton onPress={() => {}}>FadeIn</ThemeButton>
-      <ThemeButton onPress={() => {}}>FadeOut</ThemeButton>
+    <ThemeView margin className="flex-1 items-center justify-center gap-y-5">
+      <Animated.View
+        className="bg-light-secondary dark:bg-dark-secondary rounded-xl w-[100px] h-[100px]"
+        style={{ opacity: animatedOpacity }}
+      />
+
+      <ThemeButton onPress={fadeIn}>FadeIn</ThemeButton>
+      <ThemeButton onPress={fadeOut}>FadeOut</ThemeButton>
     </ThemeView>
   );
 };
