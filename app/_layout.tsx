4c0ace51fcc1b7323a9cadc60d185b26b9572ffd
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-02-18 19:43:00 +0100

    colores

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import "../global.css"
+import { Text } from 'react-native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -29,10 +30,10 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <Text className="text-3xl">Hola mundo</Text>
+      {/* <Stack>
+
+      </Stack> */}
       <StatusBar style="auto" />
     </ThemeProvider>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,28 @@
+import { Colors } from "./constants/Colors";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      light: {
+        primary: Colors.light.primary,
+        secondary: Colors.light.secondary,
+        tertiary: Colors.light.tertiary,
+        background: Colors.light.background,
+        text: Colors.light.text,
+        success: Colors.light.success,
+      },
+      dark: {
+        primary: Colors.dark.primary,
+        secondary: Colors.dark.secondary,
+        tertiary: Colors.dark.tertiary,
+        background: Colors.dark.background,
+        text: Colors.dark.text,
+        success: Colors.dark.success,
+      },
+    },
   },
   plugins: [],
-}
-
+};
