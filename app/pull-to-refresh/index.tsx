c3ddf37e078ed1ae467c75e95b6ab896a08d399d
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-02-19 12:23:55 +0100

    pull to refresh

--- app/pull-to-refresh/index.tsx
+++ app/pull-to-refresh/index.tsx
@@ -1,10 +1,34 @@
-import { View, Text } from 'react-native';
+import ThemeText from "@/components/ui/ThemeText";
+import ThemeView from "@/components/ui/ThemeView";
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { useState } from "react";
+import { RefreshControl, ScrollView } from "react-native";
 
 const PullToRefreshScreen = () => {
+  const primaryColor = useThemeColor({}, "primary");
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  };
+
   return (
-    <View>
-      <Text>PullToRefreshScreen</Text>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          colors={[primaryColor]}
+        />
+      }
+    >
+      <ThemeView>
+        <ThemeText>PullToRefreshScreen</ThemeText>
+      </ThemeView>
+    </ScrollView>
   );
 };
 export default PullToRefreshScreen;
